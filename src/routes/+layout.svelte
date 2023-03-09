4e242f5b92e31ecb4b8a6b57a715dcb9682f977d
Steve Alvarado <s.alvarado208@gmail.com>
2023-03-09 01:08:51 -0600

    Added Tailwind and Skeleton for UI and created login button on main page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-modern.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">(icon)</svelte:fragment>
+			(title)
+			<svelte:fragment slot="trail">
+				<div class="pr-4">
+					<LightSwitch />
+				</div>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
+	<!-- <svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment> -->
+	<!-- <svelte:fragment slot="pageHeader">Page Header</svelte:fragment> -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment> -->
+	<!-- <svelte:fragment slot="footer">Footer</svelte:fragment> -->
+</AppShell>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="pt-2 flex justify-center">
+  <div class="max-w-sm w-80 p-4 flex flex-col items-center">
+    <p class="my-4">
+      This is the way you take out your flustrations. Work that paint. Little trees and bushes grow however makes them happy. All you need to paint is a few tools, a little instruction, and a vision in your mind. Don't hurry. Take your time and enjoy.
+    </p>
+    <button class="btn variant-filled-primary">
+      Sign up with Google
+    </button>
+  </div>
+</div>
